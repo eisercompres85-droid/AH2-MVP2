e5b0f0922eabc31c428c8991df1430d94690d673
eisercompres85-droid <eisercompres85@gmail.com>
2026-03-24 17:35:10 -0400

    Modify API keys in config.py
    Updated API keys with new values and comments.

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
+# key de google ai studio
 API_KEYS = [
-    "AIzaSyB9zP_5IIDoW6BIjS-ojgV6n56jJ15l4r0",#la que quiero usar para esto
-    "AIzaSyCo3WkB7nbC6duzbAmA6gjt9mXjWWW8TMc", #la que cree como 2 apoyo
-    "AIzaSyAQCr34sXfy4-tLTa5unO78NeVYRGjNcOo" # y la que esta por default en mi primer proyecto
+    "AIzaSyB9zP_5IIDoW6BIjS-ojgV6n56jJ15l4r",#la que quiero usar para esto0
+    "AIzaSyCo3WkB7nbC6duzbAmA6gjt9mXjWWW8TM", #la que cree como 2 apoyoc
+    "AIzaSyAQCr34sXfy4-tLTa5unO78NeVYRGjNcO" # y la que esta por default en mi primer proyectoo
     #todas esta key son gratis
 ]
